3f3adee127a6a6aae5de63edb4ee1b225f678a2b
mayke <mayke.salles.soares@gmail.com>
2023-07-09 00:17:58 -0300

    primeiro commit

---
+++ aula01.py
@@ -0,0 +1,15 @@
+#ATIVIDADE CONDICIONAL - PYTHON
+
+"""1. Faça um algoritmo para determinar se um número é positivo:
+● Solicite ao usuário que insira um número.
+● Verifique se o número é maior que zero.
+● Se for maior que zero, exiba uma mensagem informando que o número é positivo.
+Caso contrário, informe que não é positivo."""
+
+numero = int(input("Digite um número! "))
+
+if numero >= 0:
+    print("Este núemro é posistivo.")
+else:
+    print("Este número é negativo.")
+
