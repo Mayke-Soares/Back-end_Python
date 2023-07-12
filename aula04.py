a432d0ef2a873687efaf7dd263003d7be4ffe2a9
mayke <mayke.salles.soares@gmail.com>
2023-07-11 23:44:18 -0300

    mais um commit

--- aula04.py
+++ aula04.py
@@ -12,4 +12,3 @@ if idade >= 18:
     print("Você é maior de idade! ")
 else:
     print("Você é menor de idade! ")
-
---
+++ aula05.py
@@ -0,0 +1,16 @@
+#ATIVIDADE CONDICIONAL - PYTHON
+
+"""5. Receba a idade de uma pessoa e retorne se ela é apta para votar:
+● Solicite ao usuário que insira sua idade.
+● Verifique se a idade é maior ou igual a 16.
+● Se for maior ou igual a 16, exiba uma mensagem informando que a pessoa é apta para
+votar. Caso contrário, informe que não é apta."""
+
+idade = int(input("Digite sua idade! "))
+
+if idade >= 16:
+    print("Você está apto à vontar. ")
+else: 
+    print("Você não está apto à votar. ")
+
+    
